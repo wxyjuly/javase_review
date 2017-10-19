9c8bcf9a7b08ea4805bc6d4e825d88722dc52ba4
LeonXi <xiyang@asiainfo.com>
2017-10-19 17:01:13 +0800

    add package

---
+++ src/main/java/org/leonxi/javase/zhanglong/day1/oop/HelloWorld.java
@@ -0,0 +1,27 @@
+/**
+ * Project Name:javase_review_20171005
+ * File Name:HelloWorld.java
+ * Package Name:org.leonxi.javase.day01.basic
+ * Date:2017年10月5日上午10:23:24
+ * Copyright (c) 2017, 【Leon Xi】 All Rights Reserved.
+ *
+*/
+
+package org.leonxi.javase.zhanglong.day1.oop;
+/**
+ * ClassName:HelloWorld <br/>
+ * Date:     2017年10月5日 上午10:23:24 <br/>
+ * @author   Leon Xi
+ * @version  v1.0
+ * @see 	 
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+		System.out.println("hello java, I came back...");
+	}
+
+}
+
