38cfa9d19e05e6a89b07bb7010b29927b630c689
LeonXi <cdxiyang@gmail.com>
2017-10-20 11:39:27 +0800

    Annotation相关

---
+++ src/main/java/org/leonxi/javase/zhanglong/day11/annotation/demo01/OverrideTest.java
@@ -0,0 +1,30 @@
+/**
+ * Project Name:javase_review
+ * File Name:OverrideTest.java
+ * Package Name:org.leonxi.javase.zhanglong.day11.annotation.demo01
+ * Date:2017年10月20日上午11:31:21
+ * Copyright (c) 2017, 【Leon Xi】 All Rights Reserved.
+ *
+*/
+
+package org.leonxi.javase.zhanglong.day11.annotation.demo01;
+/**
+ * ClassName:OverrideTest <br/>
+ * Date:     2017年10月20日 上午11:31:21 <br/>
+ * @author   Leon Xi
+ * @version  v1.0
+ * @see 	 
+ */
+public class OverrideTest {
+
+	public static void main(String[] args) {
+		System.out.println(new OverrideTest()) ;
+	}
+
+	@Override
+	public String toString() {
+		return "OverrideTest [toString()=" + super.toString() + "]";
+	}
+
+}
+
