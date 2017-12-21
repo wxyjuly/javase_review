d29f7f9f28819987b73eeed51cc56e777b9bc87f
leon <cdxiyang@gmail.com>
2017-12-21 21:48:31 +0800

    添加代码

---
+++ src/main/java/org/leonxi/javase/liuyi/day15/collection/demo/Student.java
@@ -0,0 +1,31 @@
+package org.leonxi.javase.liuyi.day15.collection.demo;
+
+public class Student {
+	private String name ;
+	private String age ;
+	public Student(String name, String age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", age=" + age + "]";
+	}
+	
+}
