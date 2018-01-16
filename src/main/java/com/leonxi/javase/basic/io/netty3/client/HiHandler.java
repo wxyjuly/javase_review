bbbf58fb1482fca8838e88b7623be9ad55dd7c9a
LeonXi <xiyang@asiainfo.com>
2018-01-16 14:38:21 +0800

    add Netty 3

---
+++ src/main/java/com/leonxi/javase/basic/io/netty3/client/HiHandler.java
@@ -0,0 +1,65 @@
+package com.leonxi.javase.basic.io.netty3.client;
+
+import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.channel.ChannelStateEvent;
+import org.jboss.netty.channel.ExceptionEvent;
+import org.jboss.netty.channel.MessageEvent;
+import org.jboss.netty.channel.SimpleChannelHandler;
+/**
+ * 消息接受处理类
+ * ClassName: HiHandler <br/>
+ * date: 2018年1月16日 下午2:11:34 <br/>
+ * @author Leon Xi
+ * @version v1.0
+ */
+public class HiHandler extends SimpleChannelHandler {
+
+	/**
+	 * 接收消息
+	 */
+	@Override
+	public void messageReceived(ChannelHandlerContext ctx, MessageEvent e) throws Exception {
+
+		
+		String s = (String) e.getMessage();
+		System.out.println(s);
+		
+		super.messageReceived(ctx, e);
+	}
+
+	/**
+	 * 捕获异常
+	 */
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e) throws Exception {
+		System.out.println("exceptionCaught");
+		super.exceptionCaught(ctx, e);
+	}
+
+	/**
+	 * 新连接
+	 */
+	@Override
+	public void channelConnected(ChannelHandlerContext ctx, ChannelStateEvent e) throws Exception {
+		System.out.println("channelConnected");
+		super.channelConnected(ctx, e);
+	}
+
+	/**
+	 * 必须是链接已经建立，关闭通道的时候才会触发
+	 */
+	@Override
+	public void channelDisconnected(ChannelHandlerContext ctx, ChannelStateEvent e) throws Exception {
+		System.out.println("channelDisconnected");
+		super.channelDisconnected(ctx, e);
+	}
+
+	/**
+	 * channel关闭的时候触发
+	 */
+	@Override
+	public void channelClosed(ChannelHandlerContext ctx, ChannelStateEvent e) throws Exception {
+		System.out.println("channelClosed");
+		super.channelClosed(ctx, e);
+	}
+}
